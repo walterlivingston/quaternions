33834ef952187ee5f83c274d966cdb64ccbc1fe7
Walter Livingston <wblivingston18@gmail.com>
2024-08-07 09:17:09 -0500

    added all major functions

---
+++ quaternions.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def qMult(q: np.array, p:np.array) -> np.array:
+    pass
+
+def qNorm(q: np.array) -> float:
+    pass
+
+def qNormalize(q: np.array) -> np.array:
+    pass
+
+def qInverse(q: np.array) -> np.array:
+    pass
+
+def qConj(q: np.array) -> np.array:
+    pass
+
+def q2eul(q:np.array):
+    pass
+
+def eul2q(q:np.array):
+    pass
+
+def q2DCM(q:np.array):
+    pass
+
+def DCM2q(q:np.array):
+    pass
