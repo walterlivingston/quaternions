246fe0dcb4b9ffd5fc03cb4282a1d7fc146e4abc
Walter Livingston <wblivingston18@gmail.com>
2024-08-07 09:21:19 -0500

    added euler tuple and return types

--- quaternions.py
+++ quaternions.py
@@ -1,4 +1,7 @@
 import numpy as np
+from collections import namedtuple
+
+EULER321 = namedtuple('EULER321', ['r','p','y'])
 
 def qMult(q: np.array, p:np.array) -> np.array:
     pass
@@ -15,14 +18,14 @@ def qInverse(q: np.array) -> np.array:
 def qConj(q: np.array) -> np.array:
     pass
 
-def q2eul(q:np.array):
+def q2eul(q:np.array) -> EULER321:
     pass
 
-def eul2q(q:np.array):
+def eul2q(eul:EULER321) -> np.array:
     pass
 
-def q2DCM(q:np.array):
+def q2DCM(q:np.array) -> np.array:
     pass
 
-def DCM2q(q:np.array):
+def DCM2q(q:np.array) -> np.array:
     pass
